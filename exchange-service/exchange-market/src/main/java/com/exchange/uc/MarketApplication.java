c694c8c0b5ebc68f74f4507eae07ea4f5d892ea1
liulinhui <1249055292@qq.com>
2018-08-21 00:34:35 +0800

    add market and uc module

---
+++ exchange-service/exchange-market/src/main/java/com/exchange/uc/MarketApplication.java
@@ -0,0 +1,22 @@
+package com.exchange.uc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liulinhui
+ * Date: 2018/8/21
+ * Time: 0:25
+ * Description: MarketApplication
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class MarketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarketApplication.class, args);
+    }
+}
---
+++ exchange-service/exchange-user-center/src/main/java/com/exchange/uc/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.exchange.uc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liulinhui
+ * Date: 2018/8/21
+ * Time: 0:26
+ * Description: TestController
+ */
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @RequestMapping("index")
+    public String index(@RequestParam("param") String param) {
+        log.info("exchange-user-center get param..... >>>>>" + param);
+        return "hello " + param;
+    }
+}
